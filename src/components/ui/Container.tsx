8bbf1430607873f69d144df4cc941a3c4506b20e
Danil Bashirov <danilbashirov0@gmail.com>
2025-09-26 13:41:12 +0500

    Container smooth scroll

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -12,20 +12,21 @@ function Container({
   className?: string;
 }) {
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 20 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.2 }}
-      transition={{
-        duration: 0.6,
-        ease: [0.25, 0.1, 0.25, 1], // Красивая кривая Безье (стандартное "ease-in-out")
-        delay: 0.2,
-      }}
-      id={id}
-      className={cn(className, "space-y-5")}
-    >
-      {children}
-    </motion.div>
+    <div id={id}>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.2 }}
+        transition={{
+          duration: 0.6,
+          ease: [0.25, 0.1, 0.25, 1],
+          delay: 0.2,
+        }}
+        className={cn(className, "space-y-5")}
+      >
+        {children}
+      </motion.div>
+    </div>
   );
 }
 
