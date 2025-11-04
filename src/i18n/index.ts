2ac28c7acb3c15091cb711943a782298961a30cc
Danil Bashirov <danilbashirov0@gmail.com>
2025-11-04 16:38:35 +0500

    en language as default

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,24 +1,24 @@
 import i18n from "i18next";
-import {initReactI18next} from "react-i18next";
+import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import I18NextHttpBackend from "i18next-http-backend";
 
 i18n
   .use(LanguageDetector)
   .use(I18NextHttpBackend)
-  .use(initReactI18next) // подключаем react-i18next
+  .use(initReactI18next)
   .init({
     lng: "ru",
     backend: {
       loadPath: `${import.meta.env.VITE_PUBLIC_API_URL}/text/{{lng}}`,
     },
-      // resources: {
-      //     en: { translation: en },
-      //     ru: { translation: ru }
-      // },
-    fallbackLng: "ru", // язык по умолчанию
+    // resources: {
+    //     en: { translation: en },
+    //     ru: { translation: ru }
+    // },
+    fallbackLng: "en",
     interpolation: {
-      escapeValue: false, // не экранируем строки
+      escapeValue: false,
     },
   });
 
